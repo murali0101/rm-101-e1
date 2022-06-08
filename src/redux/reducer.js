fdfdbfaae710e55843d5f44753d5c4b92e3f76ca
murali0101 <muralimv0101@gmail.com>
2022-06-08 19:57:08 +0530

    Update reducer.js

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,9 +1,13 @@
+import { ADDDATA } from "./action";
+
 const init = {
   todo: [],
 };
 
 export const Reducer = (store = init, { type, payload }) => {
   switch (type) {
+    case ADDDATA:
+      return { ...store, todo: [...payload] };
     default:
       return store;
   }
