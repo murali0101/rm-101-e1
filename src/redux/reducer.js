df6ca6daa29dcde07d674e30a71e365e2d3e68e5
murali0101 <muralimv0101@gmail.com>
2022-06-08 22:06:29 +0530

    Update reducer.js

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -9,16 +9,23 @@ export const Reducer = (store = init, { type, payload }) => {
     case ADDDATA:
       return { ...store, todo: [...payload] };
     case ADDTASKDATA:
-      return { ...store, todo: [...store.todo,payload] };
-    // case ADDTASKDATA1:
-    //       return {
-    //           ...store, todo: [...store.todo].map((ele) => {
-    //               if (ele.id == payload[1]) { 
-    //                   ele.done=payload[0]
-    //               }
-    //   }) };
+      return { ...store, todo: [...store.todo, payload] };
+    case ADDTASKDATA1:
+      return {
+        ...store,
+        todo: [...store.todo].map((ele) => {
+          if (ele.id == payload[1]) {
+            ele.done = !ele.done;
+            //   console.log(ele.id)
+          }
+          return ele;
+        }),
+      };
     case DELETEDATA:
-      return { ...store, todo: [...store.todo].filter((ele)=>ele.id!=payload) };
+      return {
+        ...store,
+        todo: [...store.todo].filter((ele) => ele.id != payload),
+      };
     default:
       return store;
   }
