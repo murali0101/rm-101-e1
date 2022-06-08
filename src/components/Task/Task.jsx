4aaf519f33d8b1f60e113d7b5bb2ee0e30027c19
murali0101 <muralimv0101@gmail.com>
2022-06-08 19:37:19 +0530

    Update Task.jsx

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import Counter from "../Counter/Counter";
 import styles from "./task.module.css";
-
-const Task = () => {
+// import remove from "../../assets/remove.svg";
+const Task = ({ props }) => {
   // NOTE: do not delete `data-testid` key value pair
+  const { id, text, done, count } = props;
+  // console.log(props)
   return (
     <li data-testid="task" className={styles.task}>
       <input type="checkbox" data-testid="task-checkbox" />
-      <div data-testid="task-text"></div>
+      <div data-testid="task-text">{text}</div>
       {/* Counter here */}
-      <button data-testid="task-remove-button"></button>
+      <Counter/>
+      <button data-testid="task-remove-button">Delete</button>
     </li>
   );
 };
