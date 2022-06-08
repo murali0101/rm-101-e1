101f3a6cb2ad93f41a5ea54211a6c03a15a1e5c4
murali0101 <muralimv0101@gmail.com>
2022-06-08 19:56:56 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,5 @@
-import { legacy_createStore } from "redux";
+import { applyMiddleware, legacy_createStore } from "redux";
+import thunk from "redux-thunk";
+import { Reducer } from "./reducer";
 
-
-
-
-
-export const store = legacy_createStore()
+export const store = legacy_createStore(Reducer, applyMiddleware(thunk));
