3d2902e0d79a5d78932889f73413a8469b42caf2
murali0101 <muralimv0101@gmail.com>
2022-06-08 20:10:15 +0530

    Update Tasks.jsx

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./tasks.module.css";
 import data from "../../data/tasks.json";
 import Task from "../Task/Task";
+import { useDispatch, useSelector } from "react-redux";
+import { addData } from "../../redux/action";
 const Tasks = () => {
   // NOTE: do not delete `data-testid` key value pair
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(addData(data));
+  }, []);
+  const todoData = useSelector((store) => store.todo);
+  console.log('todoData:', todoData)
   return (
     <>
       <ul data-testid="tasks" className={styles.tasks}>
         {/* Task List */}
-        {data.map((ele, ind) => (
+        {todoData.map((ele, ind) => (
           <Task props={ele} key={ele.id} />
         ))}
       </ul>
