a2c4a5eb1fd43fc644619be208b80a8ebb5b72de
murali0101 <muralimv0101@gmail.com>
2022-06-08 20:10:05 +0530

    Update action.js

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,6 +1,11 @@
 export const ADDDATA = "ADDDATA";
+// export const ADDCOUNT = "ADDCOUNT";
 
 export const addData = (val) => {
     console.log(val)
   return { type: ADDDATA, payload: val };
 };
+// export const addcount = (val) => {
+//     console.log(val)
+//   return { type: ADDDATA, payload: val };
+// };
