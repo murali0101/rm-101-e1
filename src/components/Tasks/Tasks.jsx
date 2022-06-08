e9935d21fe7f0619ce3e0d85d7a962cff4448f9d
murali0101 <muralimv0101@gmail.com>
2022-06-08 19:10:20 +0530

    Update Tasks.jsx

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import styles from "./tasks.module.css";
-
+import data from "../../data/tasks.json";
+import Task from "../Task/Task";
 const Tasks = () => {
   // NOTE: do not delete `data-testid` key value pair
   return (
     <>
       <ul data-testid="tasks" className={styles.tasks}>
         {/* Task List */}
+        {data.map((ele, ind) => (
+          <Task key={ele.id} />
+        ))}
       </ul>
-      <div data-testid="tasks-empty" className={styles.empty}>
+      {data.length==0?<div data-testid="tasks-empty" className={styles.empty} >
         {/* Show when No Tasks are present */}
-      </div>
+        Please Add Some Thing
+      </div>:null}
     </>
   );
 };
