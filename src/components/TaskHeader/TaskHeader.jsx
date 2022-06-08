5b7fba7c8e600688f253770c8df220dfba6c82b0
murali0101 <muralimv0101@gmail.com>
2022-06-08 19:37:09 +0530

    Update TaskHeader.jsx

--- src/components/TaskHeader/TaskHeader.jsx
+++ src/components/TaskHeader/TaskHeader.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import styles from "./taskHeader.module.css";
-
+import data from "../../data/tasks.json";
 const TaskHeader = () => {
   // sample values to be replaced
-  let totalTask = 0;
-  let unCompletedTask = 0;
+  let val = data.filter((ele) => ele.done == false);
+  let totalTask = data.length || 0;
+  let unCompletedTask = val.length || 0;
 
   // NOTE: do not delete `data-testid` key value pair
   return (
     <div data-testid="task-header" className={styles.taskHeader}>
-      <b data-testid="header-remaining-task">{unCompletedTask}</b>
-      <b data-testid="header-total-task">{totalTask}</b>
+      You have <b data-testid="header-remaining-task">{unCompletedTask}</b>of
+      <b data-testid="header-total-task">{totalTask}</b> tasks remaining
     </div>
   );
 };
