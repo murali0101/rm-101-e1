c28b674c30aea7d7788f0b06a7d5296efa51133c
murali0101 <muralimv0101@gmail.com>
2022-06-08 20:10:43 +0530

    Update Counter.jsx

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./counter.module.css";
 
-const Counter = () => {
+const Counter = ({ props }) => {
   // sample value to be replaced
-  let count = 0;
+  // let count = props.count || 0;
   // NOTE: do not delete `data-testid` key value pair
+  const [track, setTrack] = useState(props.count);
+
   return (
     <div className={styles.counter}>
-      <button data-testid="task-counter-increment-button">+</button>
-      <span data-testid="task-counter-value">{count}</span>
-      <button data-testid="task-counter-decrement-button">-</button>
+      <button
+        data-testid="task-counter-increment-button"
+        onClick={() => {
+          setTrack((p) => p + 1);
+        }}
+      >
+        +
+      </button>
+      <span data-testid="task-counter-value">{track}</span>
+      <button
+        data-testid="task-counter-decrement-button"
+        onClick={() => {
+          if (track == 0) {
+            return;
+          }
+          setTrack((p) => p - 1);
+        }}
+      >
+        -
+      </button>
     </div>
   );
 };
