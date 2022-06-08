5efbfce9638392456e22b2d3e0912fefc7742886
murali0101 <muralimv0101@gmail.com>
2022-06-08 20:22:55 +0530

    Update Task.jsx

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { useDispatch } from "react-redux";
+import { deleteData } from "../../redux/action";
 import Counter from "../Counter/Counter";
 import styles from "./task.module.css";
 // import remove from "../../assets/remove.svg";
 const Task = ({ props }) => {
   const dispatch = useDispatch()
   // NOTE: do not delete `data-testid` key value pair
-  const { id, text, done, count } = props;
+  const { id, text, done, count } = props.ele;
   // console.log(props)
   return (
     <li data-testid="task" className={styles.task}>
@@ -15,7 +16,7 @@ const Task = ({ props }) => {
       {/* Counter here */}
       <Counter props={props}/>
       <button data-testid="task-remove-button" onClick={() => {
-        
+        dispatch(deleteData(id))
       }}>Delete</button>
     </li>
   );
